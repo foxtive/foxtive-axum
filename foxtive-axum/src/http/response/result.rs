14d927e11c7eb8b38c6fbf7a536f260cd23308f5
GitHub Action <action@github.com>
2026-03-11 00:22:18 +0000

    style(cargo-fmt): apply changes

--- foxtive-axum/src/http/response/result.rs
+++ foxtive-axum/src/http/response/result.rs
@@ -41,7 +41,11 @@ impl<T: Serialize> OptionResultResponseExt<T> for AppResult<T> {
         self.is_error() || self.is_empty()
     }
 
-    fn send_response<C: ResponseCodeContract, M: Into<String>>(self, code: C, msg: M) -> HttpResult {
+    fn send_response<C: ResponseCodeContract, M: Into<String>>(
+        self,
+        code: C,
+        msg: M,
+    ) -> HttpResult {
         Ok(Responder::send_msg(
             self.map_err(HttpError::AppError)?,
             code,
@@ -58,7 +62,11 @@ impl<T: Serialize> ResultResponseExt for AppResult<T> {
         }
     }
 
-    fn send_result_msg<C: ResponseCodeContract, M: Into<String>>(self, code: C, msg: M) -> HttpResult {
+    fn send_result_msg<C: ResponseCodeContract, M: Into<String>>(
+        self,
+        code: C,
+        msg: M,
+    ) -> HttpResult {
         match self {
             Ok(data) => Ok(Responder::send_msg(data, code, msg)),
             Err(err) => Err(HttpError::AppError(err)),
